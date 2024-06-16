52f3fa58109720bd3bc705682a644858203ed4ea
Adeeb <adeeb5.sadiq7@gmail.com>
2024-06-17 00:04:57 +0530

    starting with javscript

---
+++ 05. JavaScript/01. Starting with javascript/script.js
@@ -0,0 +1,13 @@
+alert("hello world")
+
+console.log("Using Java script")
+
+prompt("Type a number")
+
+var a = confirm("Leave the page?")
+
+if(a) {
+    console.log("leaving the page")
+} else {
+    console.log("Not leaving the page")
+}
