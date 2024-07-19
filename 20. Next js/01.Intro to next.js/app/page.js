0d500e91b479840af0779fd422b2bf2271421ecd
Adeeb <adeeb5.sadiq7@gmail.com>
2024-07-19 23:19:49 +0530

    next.js

---
+++ 20. Next js/01.Intro to next.js/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+   <div className="p-1">
+    <span className="italic text-lg">Home Page</span>
+    <h1 className="text-3xl">Problems Solved by Next.js:</h1>
+    <ul className="text-xl p-1">
+      <li>Full Stack Solution</li>
+      <li>File based routing</li>
+      <li>Additional features like router from next/navigation</li>
+      <li>Optimized rendering</li>
+    </ul>
+   </div>
+  );
+}
---
+++ 20. Next js/01.Intro to next.js/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-4 flex justify-center'>
+      This is NavBar
+    </div>
+  )
+}
+
+export default Navbar
