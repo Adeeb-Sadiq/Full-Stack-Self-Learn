d927d5a80dde55df591676009b4270371259df19
Adeeb <adeeb5.sadiq7@gmail.com>
2024-07-21 18:48:50 +0530

    commit

---
+++ 20. Next js/09. layouts/components/AdminNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AdminNavbar = () => {
+  return (
+    <div className='p-2 bg-blue-800 flex justify-center'>
+      <ul className='flex gap-5'>
+        <li>Home</li>
+        <li>Users</li>
+        <li>Daskboard</li>
+      </ul>
+    </div>
+  )
+}
+
+export default AdminNavbar
